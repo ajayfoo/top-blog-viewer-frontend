7a16f39498744d0d8cbbcf6b7beadef2c2522ad7
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-09 21:08:24 +0530

    style: color post header

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,14 +1,17 @@
 import PropTypes from "prop-types";
 import classes from "./style.module.css";
+import { format } from "date-fns";
 
 function Post({ post }) {
+  const updatedAt = format(post.updatedAt, "d MMM yyyy");
   return (
     <section className={classes.post}>
       <header>
+        <span className={classes.author}>{post.author}</span>
         <h1 className={classes.title}>{post.title}</h1>
-        <span className={classes["author-detail"]}>
-          by <span className={classes.author}>{post.author}</span>
-        </span>
+        <p className={classes["updated-at"]}>
+          Last updated on <time dateTime={post.updatedAt}>{updatedAt}</time>
+        </p>
       </header>
       <p className={classes.body}>{post.body}</p>
     </section>
