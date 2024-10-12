f7ec6ca578f321ab568466ee232f4026eef24f21
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-12 21:35:14 +0530

    feat: show spinner in confirm delete button after clicking it

--- src/components/ConfirmModal/index.jsx
+++ src/components/ConfirmModal/index.jsx
@@ -1,21 +1,41 @@
 import PropTypes from "prop-types";
-import { forwardRef } from "react";
+import { forwardRef, useState } from "react";
 import classes from "./style.module.css";
+import Spinner from "../Spinner";
 
 const ConfirmModal = forwardRef(function ConfirmModal(
   { message, onCancel, onConfirm },
   ref
 ) {
+  const [isProcessing, setIsProcessing] = useState(false);
+  const handleConfirmClick = async () => {
+    try {
+      setIsProcessing(true);
+      await onConfirm();
+    } finally {
+      setIsProcessing(false);
+    }
+  };
   return (
     <dialog onClose={onCancel} ref={ref} className={classes["confirm-modal"]}>
       <form method="dialog">
         <p className={classes.message}>{message}</p>
         <div className={classes["action-buttons"]}>
-          <button className={classes.cancel} type="button" onClick={onCancel}>
+          <button
+            className={classes.cancel}
+            type="button"
+            onClick={onCancel}
+            disabled={isProcessing}
+          >
             Cancel
           </button>
-          <button className={classes.confirm} type="button" onClick={onConfirm}>
-            Confirm
+          <button
+            className={classes.confirm}
+            type="button"
+            onClick={handleConfirmClick}
+            disabled={isProcessing}
+          >
+            {isProcessing ? <Spinner /> : "Confirm"}
           </button>
         </div>
       </form>
