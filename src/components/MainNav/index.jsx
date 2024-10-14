487105dd33ed9ce089f0ac391257b7eef19567d3
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-14 18:59:08 +0530

    style: style main nav position and color, etc.

--- src/components/MainNav/index.jsx
+++ src/components/MainNav/index.jsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import classes from "./style.module.css";
 
 function MainNav() {
   return (
     <nav className={classes["main-nav"]}>
       <div className={classes["nav-items"]}>
-        <Link to="/">Home</Link>
+        <NavLink
+          to="/"
+          className={({ isActive }) => (isActive ? classes.active : "")}
+        >
+          Home
+        </NavLink>
       </div>
     </nav>
   );
