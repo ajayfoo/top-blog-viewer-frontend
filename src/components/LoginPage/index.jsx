0990f150a234177444edf80a8cfde05d5e1cb3b5
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-15 22:03:43 +0530

    feat: add ui elements to login page

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -1,5 +1,66 @@
+import PropTypes from "prop-types";
+import classes from "./style.module.css";
+import { useState } from "react";
+
+function UsernameField({ initialValue = "" }) {
+  const [text, setText] = useState(initialValue);
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <section className={classes.field}>
+      <label htmlFor="username">Username</label>
+      <input
+        id="username"
+        autoComplete="username"
+        type="text"
+        value={text}
+        onChange={handleChange}
+      />
+    </section>
+  );
+}
+UsernameField.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  initialValue: PropTypes.string,
+};
+
+function PasswordField({ initialValue = "" }) {
+  const [text, setText] = useState(initialValue);
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+  return (
+    <section className={classes.field}>
+      <label htmlFor="password">Password</label>
+      <input
+        autoComplete="current-password"
+        id="password"
+        type="password"
+        value={text}
+        onChange={handleChange}
+      />
+    </section>
+  );
+}
+PasswordField.propTypes = {
+  name: PropTypes.string,
+  initialValue: PropTypes.string,
+};
+
 function LoginPage() {
-  return "login page";
+  return (
+    <main className={classes.main}>
+      <form className={classes.form}>
+        <UsernameField />
+        <PasswordField />
+        <button className={classes.login} type="button">
+          Login
+        </button>
+      </form>
+    </main>
+  );
 }
 
 export default LoginPage;
