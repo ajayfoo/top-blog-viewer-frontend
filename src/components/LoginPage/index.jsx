306b8317add16ebd1a84c323fd36b5467ccf1a31
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-15 22:22:55 +0530

    feat: add feat to login and redirect to home page

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -1,63 +1,88 @@
 import PropTypes from "prop-types";
 import classes from "./style.module.css";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function UsernameField({ initialValue = "" }) {
-  const [text, setText] = useState(initialValue);
-  const handleChange = (e) => {
-    setText(e.target.value);
-  };
+function UsernameField({ value, onChange }) {
   return (
     <section className={classes.field}>
       <label htmlFor="username">Username</label>
       <input
         id="username"
+        required
         autoComplete="username"
         type="text"
-        value={text}
-        onChange={handleChange}
+        value={value}
+        onChange={onChange}
       />
     </section>
   );
 }
 UsernameField.propTypes = {
-  name: PropTypes.string,
-  type: PropTypes.string,
-  initialValue: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
-function PasswordField({ initialValue = "" }) {
-  const [text, setText] = useState(initialValue);
-  const handleChange = (e) => {
-    setText(e.target.value);
-  };
+function PasswordField({ value, onChange }) {
   return (
     <section className={classes.field}>
       <label htmlFor="password">Password</label>
       <input
         autoComplete="current-password"
+        required
         id="password"
         type="password"
-        value={text}
-        onChange={handleChange}
+        value={value}
+        onChange={onChange}
       />
     </section>
   );
 }
 PasswordField.propTypes = {
-  name: PropTypes.string,
-  initialValue: PropTypes.string,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const url = import.meta.env.VITE_API_URL + "/auth/login";
+      const res = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+        mode: "cors",
+      });
+      if (!res.ok) {
+        throw new Error(res.status + ": " + res.statusText);
+      }
+      const jwt = await res.text();
+      const auth = "Bearer " + jwt;
+      navigate("/");
+      localStorage.setItem("auth", auth);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <main className={classes.main}>
-      <form className={classes.form}>
-        <UsernameField />
-        <PasswordField />
-        <button className={classes.login} type="button">
-          Login
-        </button>
+      <form onSubmit={handleSubmit} className={classes.form}>
+        <UsernameField value={username} onChange={handleUsernameChange} />
+        <PasswordField value={password} onChange={handlePasswordChange} />
+        <button className={classes.login}>Login</button>
       </form>
     </main>
   );
