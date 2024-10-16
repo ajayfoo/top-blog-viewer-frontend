8a79261eb53dc63a0db1e79446f132fe251db3e8
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-16 13:09:27 +0530

    feat: add feat to show error modal or invalid cred msg for login page

--- src/components/LoginPage/index.jsx
+++ src/components/LoginPage/index.jsx
@@ -1,8 +1,9 @@
 import PropTypes from "prop-types";
 import classes from "./style.module.css";
 import Spinner from "../Spinner";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import ErrorModal from "../ErrorModal";
 
 function UsernameField({ value, onChange, disabled }) {
   return (
@@ -57,6 +58,20 @@ function LoginForm() {
   const [password, setPassword] = useState("");
   const [isSending, setIsSending] = useState(false);
   const navigate = useNavigate();
+  const [error, setError] = useState(null);
+  const [isInvalidCred, setIsInvalidCred] = useState(false);
+  const errorModalRef = useRef(null);
+
+  useEffect(() => {
+    if (!error) return;
+    errorModalRef.current.showModal();
+  }, [error]);
+
+  const handleErrorModalClose = () => {
+    errorModalRef.current.close();
+    setError(null);
+  };
+
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
   };
@@ -69,6 +84,7 @@ function LoginForm() {
     try {
       const url = import.meta.env.VITE_API_URL + "/auth/login";
       setIsSending(true);
+      setIsInvalidCred(false);
       const res = await fetch(url, {
         method: "POST",
         headers: {
@@ -77,15 +93,21 @@ function LoginForm() {
         body: JSON.stringify({ username, password }),
         mode: "cors",
       });
+      if (res.status === 401) {
+        setIsInvalidCred(true);
+        return;
+      }
       if (!res.ok) {
         throw new Error(res.status + ": " + res.statusText);
       }
+      setIsInvalidCred(false);
       const jwt = await res.text();
       const auth = "Bearer " + jwt;
       navigate("/");
       localStorage.setItem("auth", auth);
     } catch (err) {
       console.error(err);
+      setError("Something went wrong");
     } finally {
       setIsSending(false);
     }
@@ -103,6 +125,11 @@ function LoginForm() {
           value={password}
           onChange={handlePasswordChange}
         />
+        {isInvalidCred && (
+          <span className={classes["invalid-cred-msg"]}>
+            Invalid username or password
+          </span>
+        )}
         <div className={classes["action-buttons"]}>
           <button disabled={isSending} className={classes.login}>
             {isSending ? <Spinner className={classes["spinner"]} /> : "Login"}
@@ -116,6 +143,13 @@ function LoginForm() {
           )}
         </div>
       </form>
+      {error && (
+        <ErrorModal
+          message={error}
+          onClose={handleErrorModalClose}
+          ref={errorModalRef}
+        />
+      )}
     </main>
   );
 }
