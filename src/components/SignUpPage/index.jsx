b7a9a94124ef1030fcd5d0816b567975c3df38b2
Ajay Karthikesan <113184147+ajayfoo@users.noreply.github.com>
2024-10-18 11:35:10 +0530

    feat: add basic custom validation for username in sign up

--- src/components/SignUpPage/index.jsx
+++ src/components/SignUpPage/index.jsx
@@ -1,138 +1,200 @@
 import classes from "./style.module.css";
 import Spinner from "../Spinner";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useReducer, useRef, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import ErrorModal from "../ErrorModal";
+import signUpFormReducer from "./reducer.js";
+
+const isAlphaNumericOrUnderscore = (str) => {
+  let code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (
+      !(code > 47 && code < 58) && // numeric (0-9)
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123) && // lower alpha (a-z)
+      !(code === 95)
+    ) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const handleSubmitEvent = async (e, dispatch, state, navigate) => {
+  e.preventDefault();
+  const { username, password, confirmPassword } = state;
+  try {
+    const url = import.meta.env.VITE_API_URL + "/auth/sign-up";
+    dispatch({
+      type: "sending",
+    });
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password, confirmPassword }),
+      mode: "cors",
+    });
+    if (!res.ok) {
+      throw new Error(res.status + ": " + res.statusText);
+    }
+    const jwt = await res.text();
+    const auth = "Bearer " + jwt;
+    navigate("/");
+    localStorage.setItem("auth", auth);
+  } catch (err) {
+    console.error(err);
+    dispatch({
+      type: "errored",
+      text: "Something went wrong",
+    });
+  } finally {
+    dispatch({
+      type: "sent",
+    });
+  }
+};
+
+const updateUsernameError = (username, set) => {
+  if (username.length === 0) {
+    set("Required");
+  } else if (username.length < 6 || username.length > 36) {
+    set("Must be 6-36 characters long");
+  } else if (!isAlphaNumericOrUnderscore(username)) {
+    set("Must contain only alphabets, numbers or underscore");
+  } else {
+    set(null);
+  }
+};
 
 function SignUpForm() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [isSending, setIsSending] = useState(false);
+  const [state, dispatch] = useReducer(signUpFormReducer, {
+    username: "",
+    password: "",
+    confirmPassword: "",
+    isSending: false,
+    error: null,
+  });
+  const [usernameError, setUsernameError] = useState(null);
+  const [showUsernameError, setShowUsernameError] = useState(false);
   const navigate = useNavigate();
-  const [error, setError] = useState(null);
-  const [isInvalidCred, setIsInvalidCred] = useState(false);
   const errorModalRef = useRef(null);
 
   useEffect(() => {
-    if (!error) return;
+    if (!state.error) return;
     errorModalRef.current.showModal();
-  }, [error]);
+  }, [state.error]);
 
   const handleErrorModalClose = () => {
     errorModalRef.current.close();
-    setError(null);
+    dispatch({
+      type: "error_cleared",
+    });
   };
 
+  const handleUsernameBlur = () => {
+    setShowUsernameError(true);
+  };
   const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
+    const value = e.target.value;
+    dispatch({
+      type: "username_edited",
+      text: value,
+    });
+    updateUsernameError(value, setUsernameError);
   };
 
   const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+    dispatch({
+      type: "password_edited",
+      text: e.target.value,
+    });
   };
 
   const handleConfirmPasswordChange = (e) => {
-    setConfirmPassword(e.target.value);
+    dispatch({
+      type: "confirm_password_edited",
+      text: e.target.value,
+    });
   };
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const url = import.meta.env.VITE_API_URL + "/auth/sign-up";
-      setIsSending(true);
-      setIsInvalidCred(false);
-      const res = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password, confirmPassword }),
-        mode: "cors",
-      });
-      if (res.status === 401) {
-        setIsInvalidCred(true);
-        return;
-      }
-      if (!res.ok) {
-        throw new Error(res.status + ": " + res.statusText);
-      }
-      setIsInvalidCred(false);
-      const jwt = await res.text();
-      const auth = "Bearer " + jwt;
-      navigate("/");
-      localStorage.setItem("auth", auth);
-    } catch (err) {
-      console.error(err);
-      setError("Something went wrong");
-    } finally {
-      setIsSending(false);
-    }
+    await handleSubmitEvent(e, dispatch, state, navigate);
   };
+
+  const disableAllFields = state.isSending;
   return (
     <main className={classes.main}>
       <form onSubmit={handleSubmit} className={classes.form}>
         <section className={classes.field}>
           <label
-            className={isSending ? classes.disabled : ""}
+            className={disableAllFields ? classes.disabled : ""}
             htmlFor="username"
           >
             Username
           </label>
           <input
-            disabled={isSending}
+            disabled={disableAllFields}
             id="username"
             required
             autoComplete="username"
             type="text"
-            value={username}
+            value={state.username}
+            minLength={6}
+            maxLength={36}
             onChange={handleUsernameChange}
+            onBlur={handleUsernameBlur}
           />
+          {showUsernameError && (
+            <span className={classes.error}>{usernameError}</span>
+          )}
         </section>
         <section className={classes.field}>
           <label
-            className={isSending ? classes.disabled : ""}
+            className={disableAllFields ? classes.disabled : ""}
             htmlFor="password"
           >
             Password
           </label>
           <input
-            disabled={isSending}
+            disabled={disableAllFields}
             autoComplete="new-password"
             required
             id="password"
             type="password"
-            value={password}
+            value={state.password}
             onChange={handlePasswordChange}
           />
         </section>
         <section className={classes.field}>
           <label
-            className={isSending ? classes.disabled : ""}
+            className={disableAllFields ? classes.disabled : ""}
             htmlFor="confirm-new-password"
           >
             Confirm Password
           </label>
           <input
             autoComplete="new-password"
-            disabled={isSending}
+            disabled={disableAllFields}
             required
             id="confirm-new-password"
             type="password"
-            value={confirmPassword}
+            value={state.confirmPassword}
             onChange={handleConfirmPasswordChange}
           />
         </section>
-        {isInvalidCred && (
-          <span className={classes["invalid-cred-msg"]}>
-            Invalid username or password
-          </span>
-        )}
         <div className={classes["action-buttons"]}>
-          <button disabled={isSending} className={classes.login}>
-            {isSending ? <Spinner className={classes["spinner"]} /> : "Sign Up"}
+          <button disabled={disableAllFields} className={classes.login}>
+            {disableAllFields ? (
+              <Spinner className={classes["spinner"]} />
+            ) : (
+              "Sign Up"
+            )}
           </button>
-          {isSending ? (
+          {disableAllFields ? (
             <span className={classes["disabled-sign-up"]}>Login</span>
           ) : (
             <Link className={classes["sign-up"]} to="/auth/login">
@@ -141,9 +203,9 @@ function SignUpForm() {
           )}
         </div>
       </form>
-      {error && (
+      {state.error && (
         <ErrorModal
-          message={error}
+          message={state.error}
           onClose={handleErrorModalClose}
           ref={errorModalRef}
         />
---
+++ src/components/SignUpPage/reducer.js
@@ -0,0 +1,63 @@
+const defaultState = {
+  username: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const signUpFormReducer = (state, action) => {
+  switch (action.type) {
+    case "username_edited": {
+      return {
+        ...state,
+        username: action.text,
+      };
+    }
+    case "password_edited": {
+      return {
+        ...state,
+        password: action.text,
+      };
+    }
+    case "confirm_password_edited": {
+      return {
+        ...state,
+        confirmPassword: action.text,
+      };
+    }
+    case "sending": {
+      return {
+        ...state,
+        sending: true,
+      };
+    }
+    case "sent": {
+      return {
+        ...state,
+        sending: false,
+      };
+    }
+    case "errored": {
+      return {
+        ...state,
+        error: action.text,
+      };
+    }
+    case "error_cleared": {
+      return {
+        ...state,
+        error: null,
+      };
+    }
+    case "submit": {
+      return {
+        ...state,
+        error: null,
+      };
+    }
+    default: {
+      throw new Error("Unknown action: " + action.type);
+    }
+  }
+};
+
+export default signUpFormReducer;
